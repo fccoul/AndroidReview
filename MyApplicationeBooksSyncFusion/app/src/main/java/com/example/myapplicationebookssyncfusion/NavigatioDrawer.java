d70314c58c735d626091fe92d0987a70e992da41
fccoul <fccoul@gmail.com>
2020-07-09 01:39:58 +0200

    suite navifgationDrawer

--- MyApplicationeBooksSyncFusion/app/src/main/java/com/example/myapplicationebookssyncfusion/NavigatioDrawer.java
+++ MyApplicationeBooksSyncFusion/app/src/main/java/com/example/myapplicationebookssyncfusion/NavigatioDrawer.java
@@ -6,18 +6,30 @@ import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class NavigatioDrawer extends AppCompatActivity {
+public class NavigatioDrawer extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
 
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+
+
+    private Fragment NewsFragment;
+    private Fragment BalanceFragment;
+    private Fragment TransportFragment;
+
+    //FOR EEACH Fragment  -identify by one number*
+    private static  final int FRAGMENT_NEWS=0;
+    private static  final int FRAGMENT_BALANCE=1;
+    private static  final int FRAGMENT_TRANSPORT=2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +40,19 @@ public class NavigatioDrawer extends AppCompatActivity {
         this.configureToolBar();
         this.configureDrawerLayout();;
         this.configureNavigationView();
+
+        //-by default show First Fragment
+        this.showDefaultFragment();
+    }
+
+    private void showDefaultFragment() {
+        Fragment visibleFragement=getSupportFragmentManager().findFragmentById(R.id.frmLayout);
+        if(visibleFragement==null) {
+            //-Show News fragment
+            this.showFragment(FRAGMENT_NEWS);
+            //-Mark a selected the menu item corresponding to fragment
+            this.navigationView.getMenu().getItem(0).setChecked(true);
+        }
     }
 
     @Override
@@ -42,7 +67,7 @@ public class NavigatioDrawer extends AppCompatActivity {
         }
     }
 
-
+    @Override
     public boolean onNavigationItemSelected(MenuItem item)
     {
         //4- handle Navigation item Click
@@ -50,9 +75,14 @@ public class NavigatioDrawer extends AppCompatActivity {
 
         switch(id)
         {
-            case R.id.connexion:break;
-            case R.id.balance:break;
-            case R.id.transport:break;
+            case R.id.connexion:
+               // Log.d(getClass().getSimpleName(),"clicked Connexion");
+                this.showFragment(FRAGMENT_NEWS);
+                break;
+            case R.id.balance:this.showFragment(FRAGMENT_BALANCE);
+                break;
+            case R.id.transport:this.showFragment(FRAGMENT_TRANSPORT);
+                break;
             default:break;
         }
 
@@ -84,8 +114,53 @@ public class NavigatioDrawer extends AppCompatActivity {
     private void configureNavigationView()
     {
         this.navigationView=(NavigationView)findViewById(R.id.navigView);
-        navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
+      private void showFragment(int fragmentIdentifier)
+      {
+          switch(fragmentIdentifier)
+          {
+              case FRAGMENT_NEWS:this.showNewsFragment();
+              break;
+              case FRAGMENT_BALANCE:this.showBalanceFragment();
+              break;
+              case FRAGMENT_TRANSPORT:this.showTransportFragment();
+              break;
+              default:break;
+          }
+      }
 
+//Create each fragment page and show it
+      private void showNewsFragment()
+      {
+          if(this.NewsFragment==null)
+              this.NewsFragment= com.example.myapplicationebookssyncfusion.NewsFragment.newInstance();
+          this.startTransactionFragment(this.NewsFragment);
+      }
+
+private void showBalanceFragment()
+{
+    if(this.BalanceFragment==null)
+        this.BalanceFragment= com.example.myapplicationebookssyncfusion.BalanceFragment.newInstance();
+    this.startTransactionFragment(this.BalanceFragment);
+}
+
+      private  void showTransportFragment()
+      {
+          if(this.TransportFragment==null)
+              this.TransportFragment= com.example.myapplicationebookssyncfusion.TransportFragment.newInstance();
+          this.startTransactionFragment(this.TransportFragment);
+      }
+
+      //-method generic that will replaceand showa fragment insidethe MainActivity frame Layout
+      private void startTransactionFragment(Fragment fragment)
+      {
+          if(!fragment.isVisible())
+          {
+              getSupportFragmentManager().beginTransaction()
+                                            .replace(R.id.frmLayout,fragment)
+                                            .commit();
+          }
+      }
 }
